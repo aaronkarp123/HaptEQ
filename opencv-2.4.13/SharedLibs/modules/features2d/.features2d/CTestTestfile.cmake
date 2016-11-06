9088ce86764118375f4ae0240b8cec79f0d45feb
Aaron Karp <aaronkarp123@gmail.com>
2016-11-05 19:26:29 -0500

    Cleaned up working directory, deleted unnecessary libraries

--- opencv-2.4.13/SharedLibs/modules/features2d/.features2d/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aaronkarp/Documents/Research/Summer2016/GraphingPad/opencv-2.4.13/modules/features2d
-# Build directory: /Users/aaronkarp/Documents/Research/Summer2016/GraphingPad/opencv-2.4.13/SharedLibs/modules/features2d/.features2d
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- opencv-2.4.13/StaticLibs/modules/viz/.viz/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aaronkarp/Documents/Research/Summer2016/GraphingPad/opencv-2.4.13/modules/viz
-# Build directory: /Users/aaronkarp/Documents/Research/Summer2016/GraphingPad/opencv-2.4.13/StaticLibs/modules/viz/.viz
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
